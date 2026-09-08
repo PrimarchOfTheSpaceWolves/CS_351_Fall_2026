070785da09b0ce77dae8d2db226469a70bab5cdc
Dr. Michael J. Reale <realemj@sunypoly.edu>
2026-09-08 14:30:23 -0400

    feat: return basic response

--- profexercise01/server.js
+++ profexercise01/server.js
@@ -4,7 +4,16 @@ const server = http.createServer((req,res) => {
     console.log("** HTTP REQUEST ********************");
     console.log(`${req.method} ${req.url} HTTP/${req.httpVersion}`);
     console.log("Headers:", req.headers);
-    
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+
+    console.log("** HTTP RESPONSE *******************");
+    console.log("Status:", res.statusCode);
+    console.log("Content-Type:", res.getHeader("Content-Type"));
+    console.log("************************************");
+
+    res.end(`You requested: ${req.url}`);
 });
 
 server.listen(3000, () => {
