74243d8274a0363bbaba148d73284191b21f23bb
Dr. Michael J. Reale <realemj@sunypoly.edu>
2026-09-17 15:31:43 -0400

    feat: add dynamic boilerplate

---
+++ profexercise03-dynamic/server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer((req,res) => {
+    res.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+    res.end("UNDER CONSTRUCTION");
+});
+
+server.listen(3000, () => {
+    console.log("Server started http://localhost:3000...");
+});
